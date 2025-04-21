31788f1f109bc6ae9c755df7ceee70dfb45916b4
Andriy <andriy.andriyan1@gmail.com>
2025-04-21 11:22:31 +0700

    chore: update content blog file

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,14 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  loader: glob({ pattern: '**/*.mdx', base: './src/content/blog' }),
+  loader: glob({
+    pattern: '**/*.mdx',
+    base: './src/content/blog',
+    generateId: ({ entry }) => {
+      const [, slug] = entry.replace('.mdx', '').split('--');
+      return slug;
+    }
+  }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
